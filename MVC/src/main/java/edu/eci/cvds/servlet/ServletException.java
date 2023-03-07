6f4bd67b0f8c60bfd26603c3440473d40888b6bc
unknown <santiago.forero-y@labinfo.is.escuelaing.edu.co>
2023-03-07 15:42:38 -0500

    avance parte II

--- MVC/src/main/java/edu/eci/cvds/servlet/ServletException.java
+++
@@ -1,5 +0,0 @@
-package edu.eci.cvds.servlet;
-
-public class ServletException extends Exception{
-    
-}
---
+++ MVC/src/main/java/edu/eci/cvds/servlet/model/Todo.java
@@ -0,0 +1,23 @@
+package edu.eci.cvds.servlet.model;
+
+public class Todo {
+    public Todo(){
+
+    }
+
+    public String getUserId(){
+        return null;
+    }
+
+    public String getId(){
+        return null;
+    }
+
+    public String getTitle(){
+        return null;
+    }
+
+    public String getCompleted(){
+        return null;
+    }
+}
